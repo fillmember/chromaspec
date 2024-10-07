0bb665358b1aae87b20dbbfbc081959db6f55f70
ten <fillmember@gmail.com>
2024-10-07 13:21:39 +0200

    fix: change luminance to lightness

--- src/atoms/applicationState.ts
+++ src/atoms/applicationState.ts
@@ -11,7 +11,7 @@ export enum EnumViewDataPoint {
 
 export const uiDataEnumViewDataPoint = [
   { type: EnumViewDataPoint.ScaleLevel, label: "Scale Level" },
-  { type: EnumViewDataPoint.LCH_L, label: "LCH Luminance" },
+  { type: EnumViewDataPoint.LCH_L, label: "LCH Lightness" },
   { type: EnumViewDataPoint.LCH_C, label: "LCH Chroma" },
   { type: EnumViewDataPoint.LCH_H, label: "LCH Hue" },
   { type: EnumViewDataPoint.Hex, label: "Hex" },
